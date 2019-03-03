769246b16e3d669feceb51a1fb4cd2df6401956d
Peter Henry <peterhenry20@live.com>
2019-03-03 11:08:46 -0500

    Added api/backend project

---
+++ api/AssetStore.Business/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AssetStore.Business
+{
+    public class Class1
+    {
+    }
+}
---
+++ api/AssetStore.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AssetStore.Data
+{
+    public class Class1
+    {
+    }
+}
