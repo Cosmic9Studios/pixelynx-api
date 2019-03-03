82fbc3a4daf5c58e95db464a88fffc43e9cb5725
Peter Henry <peterhenry20@live.com>
2019-03-03 10:57:10 -0500

    Added vue.js to project

---
+++ app/src/components/AssetBrowser.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-card>
+        <v-img
+          src="https://cdn.vuetifyjs.com/images/cards/desert.jpg"
+          aspect-ratio="2.75"
+        ></v-img>
+
+        <v-card-title primary-title>
+          <div>
+            <h3 class="headline mb-0">Kangaroo Valley Safari</h3>
+            <div> test text </div>
+          </div>
+        </v-card-title>
+
+        <v-card-actions>
+          <v-btn flat color="orange">Share</v-btn>
+          <v-btn flat color="orange">Explore</v-btn>
+        </v-card-actions>
+    </v-card>
+</template>
---
+++ app/src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <!--<HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>-->
+    <AssetBrowser />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import AssetBrowser from '@/components/AssetBrowser.vue'; // @ is an alias to /src
+
+@Component({
+  components: {
+    HelloWorld,
+    AssetBrowser
+  }
+})
+export default class Home extends Vue {}
+</script>
