6b4b237f58207d4ea089b3e961a7a8eda723c466
Peter Henry <peterhenry20@live.com>
2019-03-22 02:17:47 -0400

    Added default route

---
+++ api/AssetStore.Api/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AssetStore.Api.Controllers
+{
+    [Route("")]
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get() {
+            return Ok();
+        }
+    }
+}
