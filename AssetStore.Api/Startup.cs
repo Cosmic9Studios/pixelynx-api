50a4162657c8e34bf0b564d07a889fd19925cdbc
Peter Henry <peterhenry20@live.com>
2019-04-26 00:42:15 -0400

    fix(startup): Added MVC support

--- AssetStore.Api/Startup.cs
+++ AssetStore.Api/Startup.cs
@@ -32,6 +32,7 @@ namespace AssetStore.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
             services.AddGraphQL(sp => Schema.Create(c =>
             {
                 c.RegisterServiceProvider(sp);
@@ -56,6 +57,7 @@ namespace AssetStore.Api
             app.UseGraphiQL();
             app.UseVoyager();
             app.UsePlayground();
+            app.UseMvc();
         }
     }
 }
