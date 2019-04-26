da034bf441da5c40b74eab1eaa5a27f1ebc28f4e
Peter Henry <peterhenry20@live.com>
2019-04-25 21:29:21 -0400

    fix(release): Fixed semantic release and removed websockets

--- api/AssetStore.Api/Startup.cs
+++ api/AssetStore.Api/Startup.cs
@@ -50,7 +50,6 @@ namespace AssetStore.Api
             }
 
             app
-                .UseWebSockets()
                 .UseGraphQL()
                 .UseGraphiQL()
                 .UseMvc();
