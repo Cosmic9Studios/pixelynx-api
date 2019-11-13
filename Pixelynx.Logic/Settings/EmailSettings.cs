304ec8d41ebe52181bb6bbc7f123f74408ca097d
Peter Henry <peterhenry20@live.com>
2019-11-12 22:53:11 -0500

    feat(Email): Added email service

---
+++ Pixelynx.Logic/Settings/EmailSettings.cs
@@ -0,0 +1,11 @@
+namespace Pixelynx.Logic.Settings
+{
+    public class EmailSettings
+    {
+        public string Server { get; set; }
+        public int Port { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Sender { get; set; }
+    }
+}
--- vault-entrypoint.sh
+++ vault-entrypoint.sh
@@ -7,4 +7,5 @@ vault login token
 vault secrets disable secret
 vault secrets enable -path=secret -version=1 kv
 vault kv put secret/ConnectionStrings Pixelynx="Server=127.0.0.1;Port=5432;Database=postgres;User Id=postgres;Password=devpass;"
+vault kv put secret/Auth JWTSecret="bfe222e7-b5eb-4d31-91df-c30797324fbc"
 tail -f /dev/null
