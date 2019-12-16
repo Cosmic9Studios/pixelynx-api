824638430f645d9e5b84936106d91099690c1f4d
Peter Henry <peterhenry20@live.com>
2019-12-16 17:51:52 -0500

    feat(blob): Added code for GCStorage; Added directory param

--- Pixelynx.Data/BlobStorage/IBlobStorage.cs
+++ Pixelynx.Data/BlobStorage/IBlobStorage.cs
@@ -1,6 +1,6 @@
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using Amazon.S3.Model;
 
 namespace Pixelynx.Data.BlobStorage
 {
@@ -10,8 +10,15 @@ namespace Pixelynx.Data.BlobStorage
         /// Lists all the objects in the bucket.
         /// </summary>
         /// <param name="bucket">The name of the bucket to parse</param>
-        Task<IEnumerable<BlobObject>> ListObjects(string bucket);
+        Task<IEnumerable<BlobObject>> ListObjects(string bucket, string directory = "");
 
-        Task<string> UploadFileToBucket(string bucket, string fileName, byte[] fileContent);
+        /// <summary>
+        /// Uploads a file to a specified bucket.
+        /// </summary>
+        /// <param name="bucket">The name of the bucket to place the file</param>
+        /// <param name="directory">The directory to store the file</param>
+        /// <param name="fileName">The name and extension of the file being uploaded</param>
+        /// <param name="fileContent">The file data to be uploaded</param>
+        Task<string> UploadFileToBucket(string bucket, string directory, string fileName, byte[] fileContent);
     }
 }
