07c7cf55a60feef63ca56c31b40874a20c1916c9
Peter Henry <peterhenry20@live.com>
2020-02-09 00:06:48 -0500

    chore(workflow): Added registry credentials to deployment

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -6,4 +6,5 @@ EOM
 echo $DOCKER_TOKEN | docker login --username "phenry20" --password-stdin
 terraform init -input=false
 terraform apply -input=false -var-file ./terraform.tfvars -auto-approve
+kubectl create secret generic regcred --from-file=.dockerconfigjson=~/.docker/config.json --type=kubernetes.io/dockerconfigjson
 kubectl apply -f k8s/deployment.yaml
