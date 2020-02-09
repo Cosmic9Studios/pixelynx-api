ff6dc9c984ce3d11948c847e465b38935b183c62
Peter Henry <peterhenry20@live.com>
2020-02-08 20:35:53 -0500

    chore(workflow): Added terraform init

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -7,6 +7,7 @@ EOM
 
 echo $DOCKER_TOKEN | docker login --username "phenry20" --password-stdin
 gpg --quiet --batch --yes --decrypt --passphrase="$1" --output $GOOGLE_APPLICATION_CREDENTIALS ./pixelynx.json.gpg
+terraform init
 terraform apply -var-file ./terraform.tfvars -auto-approve
 export KUBECONFIG=k8s/kubeconfig
 kubectl apply -f k8s/deployment.yaml
