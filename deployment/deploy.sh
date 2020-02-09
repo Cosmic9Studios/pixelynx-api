ffe6347aefbd2da9bde2a240035cfac7a9bed4fb
Peter Henry <peterhenry20@live.com>
2020-02-08 23:41:19 -0500

    chore(workflow): Removed sudo from kubectl

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -6,4 +6,4 @@ EOM
 echo $DOCKER_TOKEN | docker login --username "phenry20" --password-stdin
 terraform init -input=false
 terraform apply -input=false -var-file ./terraform.tfvars -auto-approve
-sudo kubectl apply -f k8s/deployment.yaml
+kubectl apply -f k8s/deployment.yaml
