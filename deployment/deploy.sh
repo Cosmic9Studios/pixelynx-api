617b726f2c0ae680b671b3c95853af0212c6758b
Peter Henry <peterhenry20@live.com>
2020-02-08 22:07:13 -0500

    chore(workflow): Added KUBECONFIG env variable

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -6,5 +6,4 @@ EOM
 echo $DOCKER_TOKEN | docker login --username "phenry20" --password-stdin
 terraform init -input=false
 terraform apply -input=false -var-file ./terraform.tfvars -auto-approve
-export KUBECONFIG=k8s/kubeconfig
 sudo kubectl apply -f k8s/deployment.yaml
