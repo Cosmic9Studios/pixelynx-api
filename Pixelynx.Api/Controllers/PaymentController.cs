827062c0a23ce2b99a75633876590d9469e51c90
Peter Henry <peterhenry20@live.com>
2020-04-02 18:21:43 -0400

    feat(payment): Added payment controller

---
+++ Pixelynx.Api/Controllers/PaymentController.cs
@@ -0,0 +1,86 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Pixelynx.Api.Requests;
+using Pixelynx.Api.Responses;
+using Pixelynx.Data.Models;
+using Stripe;
+
+namespace Pixelynx.Api.Controllers
+{
+    [Route("payment")]
+    public class PaymentController : Controller
+    {
+        private UnitOfWork unitOfWork;
+
+        public PaymentController(UnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        [Route("cards"), HttpGet]
+        public async Task<IActionResult> GetCards()
+        {
+            var service = new PaymentMethodService();
+            var userId = Guid.Parse(HttpContext.User.Identity.Name);
+            var customerId = await unitOfWork.PaymentRepository.GetCustomerId(userId);
+
+            var options = new PaymentMethodListOptions
+            {
+                Customer = customerId,
+                Type = "card",
+            };
+            var cards = service.List(
+                options
+            );
+
+            return Ok(cards.Data.Select(x => new GetCardsResponse
+            {
+                Id = x.Id,
+                Last4Digits = x.Card.Last4,
+                Brand = x.Card.Brand, 
+                ExpiryMonth = x.Card.ExpMonth,
+                ExpiryYear = x.Card.ExpYear
+            }));
+        }
+
+        [Route("wallet/card/{id}"), HttpDelete]
+        public IActionResult DeleteCard(string id)
+        {
+            var service = new PaymentMethodService();
+            service.Detach(id);
+            return Ok();
+        }
+
+        [Route("wallet"), HttpGet]
+        public async Task<IActionResult> GetWallet()
+        {
+            var userId = Guid.Parse(HttpContext.User.Identity.Name);
+            var options = new SetupIntentCreateOptions{
+                Customer = await unitOfWork.PaymentRepository.GetCustomerId(userId),
+            };
+            var service = new SetupIntentService();
+            var intent = service.Create(options);
+            return Ok(intent.ClientSecret);
+        }
+
+        [Route("credits"), HttpPost]
+        public void AddCredits(AddCreditsRequest request)
+        {
+            var options = new PaymentIntentCreateOptions
+            {
+                Amount = request.Credits,
+                Currency = "usd",
+                Metadata = new Dictionary<string, string>
+                {
+                    { "integration_check", "accept_a_payment" },
+                },
+            };
+
+            var service = new PaymentIntentService();
+            var paymentIntent = service.Create(options);
+        }
+    }
+}
--- Pixelynx.Api/Responses/UserDataResponse.cs
+++ Pixelynx.Api/Responses/UserDataResponse.cs
@@ -4,8 +4,10 @@ namespace Pixelynx.Api.Responses
 {
     public class UserDataResponse
     {
+        public Guid Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string UserName { get; set; }
+        public string Email { get; set; }
     }
 }
