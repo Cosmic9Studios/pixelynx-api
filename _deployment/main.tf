124c806787bed1145ae0b0a422adf79c52f1ca47
Peter Henry <peterhenry20@live.com>
2020-04-28 01:55:11 -0400

    chore(deployment): Renamed to _deployment

---
+++ _deployment/main.tf
@@ -0,0 +1,121 @@
+terraform {
+    backend "gcs" {
+        prefix = "gcp/k8s/pixelynx-api"
+    }
+}
+
+locals {
+    domain = var.app_environment != "Production" ? "staging.pixelynx.com" : "pixelynx.com"
+    project = var.app_environment != "Production" ? "pixelynx-staging" : "pixelynx"
+    gsa_name = "pxl-api"
+    ksa_name = "pxl-api"
+    roles = [
+        "roles/iam.serviceAccountCreator",
+        "roles/iam.serviceAccountKeyAdmin",
+        "roles/iam.serviceAccountTokenCreator",
+        "roles/container.admin",
+        "roles/storage.admin",
+        "roles/cloudsql.editor",
+        "roles/compute.viewer",
+        "roles/cloudkms.cryptoKeyEncrypterDecrypter"
+    ]
+}
+
+data "terraform_remote_state" "k8s" {
+  backend = "gcs"
+  config = {
+    bucket = var.app_environment == "Staging" ? "staging-pixelynx-state" : "pixelynx-state"
+    prefix = "gcp/app"
+  }
+}
+
+data "google_client_config" "default" {}
+
+provider "kubectl" {
+    load_config_file       = false
+    host                   = "https://${data.terraform_remote_state.k8s.outputs.endpoint}"
+    token                  = data.google_client_config.default.access_token
+    cluster_ca_certificate = data.terraform_remote_state.k8s.outputs.cluster_ca_certificate
+}
+
+provider "kubernetes" {
+    load_config_file       = false
+    host                   = "https://${data.terraform_remote_state.k8s.outputs.endpoint}"
+    token                  = data.google_client_config.default.access_token
+    cluster_ca_certificate = data.terraform_remote_state.k8s.outputs.cluster_ca_certificate
+}
+
+provider "random" {}
+
+provider "google" {
+  project = local.project
+}
+
+provider "google-beta" {
+  project = local.project
+}
+
+resource "google_service_account" "gsa" {
+  account_id   = local.gsa_name
+  display_name = "A service account for Pixelynx-Api pod"
+}
+
+resource "kubernetes_service_account" "ksa" {
+  metadata {
+    name = local.ksa_name
+    annotations = {
+      "iam.gke.io/gcp-service-account" = google_service_account.gsa.email
+    }
+  }
+}
+
+resource "google_project_iam_member" "roles" {
+  for_each = toset(local.roles)
+  role    = each.key
+  member  = "serviceAccount:${google_service_account.gsa.email}"
+}
+
+resource "google_service_account_iam_member" "sa" {
+  service_account_id = google_service_account.gsa.name
+  role    = "roles/iam.workloadIdentityUser"
+  member = "serviceAccount:${local.project}.svc.id.goog[default/${local.ksa_name}]"
+}
+
+resource "random_pet" "secret" {}
+
+resource "kubernetes_secret" "secret" {
+  metadata {
+    name = random_pet.secret.id
+  }
+
+  data = {
+    ".dockerconfigjson" = jsonencode({
+      "auths" : {
+        "https://index.docker.io/v1/" : {
+          email    = "phenry@cosmic9studios.com"
+          username = "phenry20"
+          password = var.docker_pass
+          auth     = base64encode(join(":", ["phenry20", var.docker_pass]))
+        }
+      }
+    })
+  }
+
+  type = "kubernetes.io/dockerconfigjson"
+}
+
+resource "kubectl_manifest" "deployment" {
+  yaml_body = templatefile("${path.module}/manifests/deployment.yaml", {
+    environment = var.app_environment 
+    version = var.app_version
+    secret = random_pet.secret.id
+    instance = data.terraform_remote_state.k8s.outputs.db_instance_name
+    domain = local.domain
+    serviceAccountName = local.ksa_name
+  })
+
+  depends_on = [
+    kubernetes_secret.secret,
+    google_project_iam_member.roles
+  ]
+}
---
+++ _deployment/variables.tf
@@ -0,0 +1,11 @@
+variable "app_version" {
+    type = string
+}
+
+variable "app_environment" {
+    type = string
+}
+
+variable "docker_pass" {
+    type = string
+}
