f96022da4271de0138a839f4ddbd7294113cddb2
Peter Henry <peterhenry20@live.com>
2020-05-01 19:52:41 -0400

    feat(download): Added download route

---
+++ Pixelynx.Api/Requests/DownloadRequest.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+
+namespace Pixelynx.Api.Requests
+{
+    public class DownloadRequest
+    {
+        public List<Guid> Assets {  get; set; }
+    }
+}
