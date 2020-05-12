e07c80734962d373f2a65f0404d80bb34aba652a
Peter Henry <peterhenry20@live.com>
2020-05-12 19:02:13 -0400

    feat(upload): Added uploader id to uploaded assets

--- Pixelynx.Logic/Services/UploadService.cs
+++ Pixelynx.Logic/Services/UploadService.cs
@@ -1,47 +1,79 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Pixelynx.Core;
+using Pixelynx.Core.Helpers;
+using Pixelynx.Data.BlobStorage;
+using Pixelynx.Data.Entities;
+using Pixelynx.Data.Interfaces;
 using Pixelynx.Data.Models;
+using Pixelynx.Data.Settings;
 using Pixelynx.Logic.Model;
 
 namespace Pixelynx.Logic.Services
 {
     public class UploadService
     {
-        private UnitOfWork unitOfWork;
+        private IDbContextFactory dbContextFactory;
         private ILogger<UploadService> logger;
+        private StorageSettings storageSettings;
+        private IBlobStorage blobStorage;
 
-        public UploadService(UnitOfWork unitOfWork, ILogger<UploadService> logger)
+        public UploadService(IDbContextFactory dbContextFactory, 
+            ILogger<UploadService> logger, 
+            IOptions<StorageSettings> storageSettings,
+            IBlobStorage blobStorage)
         {
-            this.unitOfWork = unitOfWork;
+            this.dbContextFactory = dbContextFactory;
             this.logger = logger;
+            this.storageSettings = storageSettings.Value;
+            this.blobStorage = blobStorage;
         }
 
-        public async Task<bool> UploadAssets(List<AssetData> assetStreams, Core.Asset parent = null)
+        public async Task<bool> UploadAssets(UserEntity user, List<AssetData> assetStreams, Guid? parentId)
         {
             try 
             {
+                var context = dbContextFactory.CreateWrite();
                 foreach (var asset in assetStreams)
                 {
-                    var newAsset = new Core.Asset(asset.Metadata.Name, asset.Metadata.Type, asset.DataStream.ToArray());
-                    newAsset.Thumbnail = new Core.Thumbnail(asset.ThumbnailStream.ToArray(), asset.Metadata.Thumbnail);
-                    newAsset.Cost = asset.Metadata.Price;
-                    newAsset.Description = asset.Metadata.Description;
-                    if (parent != null)
+                    var rawAssetData = asset.DataStream.ToArray();
+                    var storageId = Guid.NewGuid();
+                    await blobStorage.UploadFileToBucket(storageSettings.AssetBucketName, storageId.ToString(), "asset.glb", rawAssetData); 
+                    if (!string.IsNullOrEmpty(asset.Metadata.Thumbnail))
                     {
-                        newAsset.Parent = parent;
+                        var name = $"thumbnail{Path.GetExtension(asset.Metadata.Thumbnail)}";
+                        await blobStorage.UploadFileToBucket(storageSettings.MediaBucketName, storageId.ToString(), name, asset.ThumbnailStream.ToArray());
                     }
 
-                    await unitOfWork.AssetRepository.CreateAsset(newAsset);
+                    var newAsset = new AssetEntity 
+                    {
+                        Id = Guid.NewGuid(), 
+                        ParentId = parentId, 
+                        UploaderId = user.Id,
+                        Name = asset.Metadata.Name,
+                        Description = asset.Metadata.Description,
+                        StorageBucket = storageSettings.AssetBucketName,
+                        StorageId = storageId,
+                        MediaStorageBucket = storageSettings.MediaBucketName,
+                        AssetType = (int)asset.Metadata.Type, 
+                        FileHash = rawAssetData.GenerateHash(),
+                        Price = asset.Metadata.Price
+                    };
+                    await context.Assets.AddAsync(newAsset);
 
                     // Model is always guaranteed to be first the list because form data comes in order (HTML SPEC)
-                    if (asset.Metadata.Type == Core.AssetType.Model)
+                    if (asset.Metadata.Type == AssetType.Model)
                     {
-                        parent = newAsset;
+                        parentId = newAsset.Id;
                     }
-                    logger.Log(LogLevel.Information, $"Uploading Asset {asset.Metadata.Name} with ParentId: {parent?.Id}");
+                    logger.Log(LogLevel.Information, $"Uploading Asset {asset.Metadata.Name} with ParentId: {parentId}");
                 }
+
+                await context.SaveChangesAsync();
                 
                 return true;
             }
