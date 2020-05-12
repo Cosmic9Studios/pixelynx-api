ee84f971d8771188f815767db08ced0de9dc1330
Peter Henry <peterhenry20@live.com>
2020-05-12 19:02:41 -0400

    chore(cleanup): Cleaned up Unit of Work

--- Pixelynx.Data/Models/UnitOfWork.cs
+++ Pixelynx.Data/Models/UnitOfWork.cs
@@ -1,22 +1,13 @@
-using Microsoft.Extensions.Options;
-using Pixelynx.Data.BlobStorage;
 using Pixelynx.Data.Interfaces;
 using Pixelynx.Data.Repositories;
-using Pixelynx.Data.Settings;
 
 namespace Pixelynx.Data.Models
 {
     public class UnitOfWork
     {
-        private static IBlobStorage storage;
-        private static StorageSettings storageSettings;
-
-        public UnitOfWork(IDbContextFactory dbContextFactory, IBlobStorage blobStorage, IOptions<StorageSettings> settings)
+        public UnitOfWork(IDbContextFactory dbContextFactory)
         {
-            storage = blobStorage;
-            storageSettings = settings.Value;
-
-            AssetRepository = new AssetRepository(dbContextFactory, storage, storageSettings);
+            AssetRepository = new AssetRepository(dbContextFactory);
             PaymentRepository = new PaymentRepository(dbContextFactory);
         }
 
