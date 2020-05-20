c785a63f37a7ddf5701cffa479c09bd91a84bafb
Peter Henry <peterhenry20@live.com>
2020-05-19 20:51:36 -0400

    chore(cleanup): Removed unneeded code

--- Pixelynx.Data/Entities/AssetEntity.cs
+++ Pixelynx.Data/Entities/AssetEntity.cs
@@ -1,10 +1,5 @@
 using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Threading.Tasks;
-using Pixelynx.Core;
-using Pixelynx.Data.BlobStorage;
 
 namespace Pixelynx.Data.Entities
 {
