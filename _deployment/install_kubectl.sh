34a1f8f0d5461c1dcbc0c03e239dc688591c3a95
Peter Henry <peterhenry20@live.com>
2020-08-03 02:21:49 -0400

    fix(deploy): Fixed deployment

--- _deployment/install_kubectl.sh
+++ _deployment/install_kubectl.sh
@@ -1,5 +1,5 @@
 mkdir -p ~/.terraform.d/plugins && \
-    curl -Ls https://api.github.com/repos/gavinbunney/terraform-provider-kubectl/releases/latest \
+    curl -Ls https://api.github.com/repos/gavinbunney/terraform-provider-kubectl/releases/28706673 \
     | jq -r ".assets[] | select(.browser_download_url | contains(\"$(uname -s | tr A-Z a-z)\")) | select(.browser_download_url | contains(\"amd64\")) | .browser_download_url" \
     | xargs -n 1 curl -Lo ~/.terraform.d/plugins/terraform-provider-kubectl && \
     chmod +x ~/.terraform.d/plugins/terraform-provider-kubectl
