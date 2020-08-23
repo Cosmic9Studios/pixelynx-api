bff8947ba540c913b5583b92625dd26bd8bb16a0
Peter Henry <peterhenry20@live.com>
2020-08-23 01:01:28 -0400

    refactor(cart): Implemented cart service

--- Pixelynx.Logic/Models/GenericResult.cs
+++ Pixelynx.Logic/Models/GenericResult.cs
@@ -2,10 +2,14 @@ using System.Collections.Generic;
 
 namespace Pixelynx.Logic.Models
 {
-    public class GenericResult<T>
+    public class GenericResult<T> : GenericResult
+    {
+        public T Data { get; set; }
+    }
+
+    public class GenericResult
     {
         public bool Succeeded { get; set; }
         public List<string> Errors { get; set; }
-        public T Data { get; set; }
     }
 }
